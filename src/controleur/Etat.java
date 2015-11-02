596a6b8692ada1bceea8f3b70e7d3dfbcbeaf3f7
chmoreau <charles.moreau@insa-lyon.fr>
2015-11-03 00:34:56 +0100

    update
    :3

--- src/controleur/Etat.java
+++ src/controleur/Etat.java
@@ -7,10 +7,10 @@ import modele.Adresse;
 public interface Etat {
 	void undo(ListeDeCmd listeCmd) throws Exception;
 	void redo(ListeDeCmd listeCmd) throws Exception;
-	void clicNoeud(Adresse adresse,Plan plan, ListeDeCmd listeCmd);
-/*	Graph chargerPlan(File plan);
-	Graph chargerLivraisons(File livraisons);
-	Graph calculerTournee(); */
-	void genererFeuilleDeRoute(String fichier);
+	void clicNoeud(Adresse adresse,Livraison livraison,Tournee tournee, ListeDeCmd listeCmd);
+	Graph chargerPlan() throws Exception;
+	Graph chargerLivraisons(Plan plan) throws Exception;
+	Graph calculerTournee(Tournee tournee); 
+	void genererFeuilleDeRoute(String fichier,Tournee tournee) throws Exception;
 	void clicDroit();
 }
