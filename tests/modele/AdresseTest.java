f2e496692e73aa9c30d86b5e4b4958609c9dcfae
unknown <fpizzo@if501-213-13.insa-lyon.fr>
2015-11-03 12:00:06 +0100

    --

--- tests/modele/AdresseTest.java
+++ tests/modele/AdresseTest.java
@@ -19,21 +19,29 @@ public class AdresseTest {
 	}
 
 	@Test
-	public void test() {
+	public void testEquals() {
 		//fail("Not yet implemented");
 		Adresse a = new Adresse(1, 2, 3);
-		Adresse b = new Adresse(1, 2, 3);
-		assert(a.equals(b));
-		
+		Adresse b = new Adresse(1, 54, 3);
+		Adresse c = new Adresse(2,5,3);
+		assertTrue("test d'egalite",a.equals(b));
+		assertFalse("test d'inegalite",a.equals(c));
 	}
 	
 	@Test
-	public void test1() {
-		//fail("Not yet implemented");
-		Adresse a = new Adresse(1, 2, 3);
-		Adresse b = new Adresse(1, 4, 3);
-		assert(a.equals(b));
+	public void testAjouterTroncon() {
+		Adresse b = new Adresse(1, 54, 3);
+		Adresse c = new Adresse(2, 5 , 3);
+		int idC = c.getId(); 
+		Troncon t = new Troncon("nom de rue", 10, 10, b, c);
+		b.ajouterTroncon(t);
+	/*	for(Troncon t : b.getTroncons()) {
+			
+		}*/
 		
 	}
+	
+	
+
 
 }
