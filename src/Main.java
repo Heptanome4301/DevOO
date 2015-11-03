ff9e05d1f64444287e35e3e05c9655c5eb7aeda2
Andrea11 <andry93.mail@gmail.com>
2015-11-03 11:01:03 +0100

    maj vue

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import vue.GraphManager;
-import vue.InterfaceGraphique;
+import vue.Fenetre;
 import modele.*;
 
 public class Main {
@@ -22,19 +21,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Plan p = new Plan();
 				
-		GraphManager.getInstance().create();
-		GraphManager.getInstance().addNode("A", 0, 0);
-		GraphManager.getInstance().addNode("B", 0, 3);
-		GraphManager.getInstance().addNode("C", 3, 0);
-		GraphManager.getInstance().addEdge("AB", "A", "B");
-		GraphManager.getInstance().addEdge("AC", "A", "C");
-		GraphManager.getInstance().addEdge("BC", "B", "C");
-		InterfaceGraphique window = new InterfaceGraphique();
-		window.linkView(GraphManager.getInstance().getView());
+		Fenetre window = new Fenetre();
 		window.display();
 		p.chargerPlan();
+		window.linkView(p);
 
-        System.out.println("Chemin de id=3 à id=1 (" + p.calculerChemin(p.getAdresse(3), p.getAdresse(1)) + ")");
+        System.out.println("Chemin de id=3 ï¿½ id=1 (" + p.calculerChemin(p.getAdresse(3), p.getAdresse(1)) + ")");
         //System.out.println(p.calculerChemin(p.getAdresse(0), p.getAdresse(p.getAdresses().size()-1)));
 		//System.out.println("Verification : tps de 0 â€¡ 1 = " + p.getAdresses().get(0).getTroncons().get0).getDuree());
 		Tournee t = p.chargerLivraison();
---
+++ src/vue/VueGrapique.java
@@ -0,0 +1,56 @@
+package vue;
+
+import java.awt.Graphics;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+import modele.Adresse;
+import modele.Plan;
+import modele.Troncon;
+
+public class VueGrapique extends JPanel implements Observer {
+	private Plan plan;
+	private Fenetre fenetre;
+	private Object element;
+	
+	public VueGrapique(Plan plan, Fenetre fenetre) {
+		this.plan = plan;
+		this.fenetre = fenetre;
+		plan.addObserver(this);
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		
+		if ( o instanceof Plan ) {
+			this.display();
+		}
+	}
+	
+	public void display() {
+		this.repaint();
+	}
+
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (element instanceof Adresse)
+        {
+        	g.fillOval( ((Adresse) element).getX(), ((Adresse)adresse).getY(), 10, 10);
+        }
+        // Draw a little square at where the mouse was clicked.
+        for (Adresse adresse : adresses) {
+        	g.fillOval(adresse.getX(), adresse.getY(), 10, 10);
+        	Collection<Troncon> troncons = adresse.getTroncons();
+        	for (Troncon troncon : troncons) {
+        		g.drawLine(adresse.getX(), adresse.getY(), troncon.getArrivee().getX(), troncon.getArrivee().getY());
+        	}
+        }
+        	
+    }
+	
+}
