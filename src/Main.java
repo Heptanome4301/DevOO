d3f7ed3d8ab803018465b84997fa424e8c909e9b
Andrea11 <andry93.mail@gmail.com>
2015-11-03 11:45:28 +0100

    maj main

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,12 @@ public class Main {
 		p.chargerPlan();
 		window.linkView(p);
 
-        System.out.println("Chemin de id=3 � id=1 (" + p.calculerChemin(p.getAdresse(3), p.getAdresse(1)) + ")");
+        /*System.out.println("Chemin de id=3 � id=1 (" + p.calculerChemin(p.getAdresse(3), p.getAdresse(1)) + ")");
         //System.out.println(p.calculerChemin(p.getAdresse(0), p.getAdresse(p.getAdresses().size()-1)));
 		//System.out.println("Verification : tps de 0 ‡ 1 = " + p.getAdresses().get(0).getTroncons().get0).getDuree());
 		Tournee t = p.chargerLivraison();
 		t.calculerTournee();
-		print_tournee(t);
+		print_tournee(t);*/
 	}
 
 }
