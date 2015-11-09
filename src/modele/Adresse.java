dc31c3812611eb5a8aaf298434c2511a891facb7
maxime-christ <maxime.christ@insa-lyon.fr>
2015-11-09 21:11:58 +0100

    javadoc de Adresse

--- src/modele/Adresse.java
+++ src/modele/Adresse.java
@@ -5,25 +5,35 @@ import java.util.Collection;
 import java.util.Observable;
 
 public class Adresse extends Observable {
-
+        
+        /**
+         * L'identifiant de l'adresse.
+         */
 	private int id;
+        /**
+         * Sa coordonnée suivant l'axe x. Utile pour la représentation graphique de cette adresse dans un plan.
+         */
 	private int x;
+        /**
+         * Sa coordonnée suivant l'axe y. Utile pour la représentation graphique de cette adresse dans un plan.
+         */
 	private int y;
+        /**
+         * La livraison associée à cette adresse, null si aucune livraison n'est prévue.
+         */
 	private Livraison livraison;
+        /**
+         * La liste des rues empruntables depuis cette adresse (représentées par des instances de la classe Troncon).
+         */
 	private Collection<Troncon> tronconsSortants;
 	
-	protected void setLivraison(Livraison l){
-		this.livraison = l;
-	}
-	
-	public Livraison getLivraison(){
-		return livraison;
-	}
-	
-	public boolean estAssocierAvecLivraison(){
-		return livraison != null;
-	}
-	
+        
+        /**
+         * Constructeur de la classe Adresse.
+         * @param id
+         * @param x
+         * @param y
+         */
 	public Adresse(int id, int x, int y) {
 		this.id = id;
 		this.x = x;
@@ -32,7 +42,10 @@ public class Adresse extends Observable {
 		livraison = null;
 	}
 	
-	
+	/**
+         * Surcharge de la méthode d'afficahge.
+         * @return une chaîne de caractère décrivant l'adresse
+         */
 	@Override
 	public String toString() {
 		String res = "Adresse " + id + "\r\n"
@@ -42,7 +55,11 @@ public class Adresse extends Observable {
 		}
 		return res;
 	}
-
+        
+        /**
+         * Surcharge du calcul de l'index de hachage afin de pouvoir utiliser un HashSet.
+         * @return l'index de hachage
+         */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -50,7 +67,12 @@ public class Adresse extends Observable {
 		result = prime * result + id;
 		return result;
 	}
-
+        
+        /**
+         * Surcharge de la méthode d'égalité
+         * @param obj la deuxième adresse à laquelle comparer la première
+         * @return true si les id sont identiques, false sinon
+         */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -64,37 +86,62 @@ public class Adresse extends Observable {
 			return false;
 		return true;
 	}
-
-
-
-
-
+        
+        /**
+         * Ajouter un tronçpn à la liste des tronçons.
+         * @param t le tronçon à ajouter.
+         */
 	public void ajouterTroncon(Troncon t) {
 		tronconsSortants.add(t);
 		notifyObservers(t);
 	}
 
+        /**
+         * Retirer un tronçon de la liste des tronçons.
+         * @param t le tronçon à retirer.
+         */
 	protected void retirerTroncon(Troncon t) {
 		tronconsSortants.remove(t);
 		notifyObservers(t);
 	}
-
+        
+        /**
+         * Accesseur de l'attribut id.
+         * @return l'id de l'adresse.
+         */
 	public int getId() {
 		return id;
 	}
 
+        /**
+         * Accesseur de l'attribut x.
+         * @return x.
+         */
 	public int getX() {
 		return x;
 	}
-
+        
+        /**
+         * Accesseur de l'attribut y.
+         * @return y.
+         */
 	public int getY() {
 		return y;
 	}
 
+        /**
+         * Accesseur de l'attribut troncons.
+         * @return la liste de tronçons sortants de l'adresse.
+         */
 	public Collection<Troncon> getTroncons() {
 		return tronconsSortants;
 	}
 
+        /**
+         * Méthode nécessaire à la mise en place du pattern visiteur.
+         * @param visiteur le visiteur.
+         * @param estEntrepot
+         */
 	public void accept(Visiteur visiteur,boolean estEntrepot) {
 		for (Troncon t : getTroncons()) {
 			t.accept(visiteur);
@@ -102,4 +149,29 @@ public class Adresse extends Observable {
 		visiteur.visite(this, estEntrepot);
 		
 	}
+        
+        /**
+         * Mutateur de l'attribut livraison.
+         * @param l la livraison à cette adresse.
+         */
+        protected void setLivraison(Livraison l){
+                this.livraison = l;
+        }
+        
+        /**
+         * Accesseur de l'attribut livraison.
+         * @return la livraison prévue à cette adresse, null sinon.
+         */
+        public Livraison getLivraison(){
+                return livraison;
+        }
+        
+        /**
+         * Renvoie un booléen traduisant le fait qu'une livraison soit prévue à cette adresse.
+         * @return true si une livraison est prévue, false sinon.
+         */
+        public boolean estAssocierAvecLivraison(){
+                return livraison != null;
+        }
+    
 }
