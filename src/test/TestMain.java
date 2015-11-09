599933631b6894da6d5a0bdb90f44fc5e80859ba
AyoubCh <aychioukh@gmail.com>
2015-11-09 18:07:55 +0100

    le bon main

---
+++ src/test/TestMain.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import controleur.Controleur;
+import vue.Fenetre;
+import xml.ExceptionXML;
+import xml.OuvreurDeFichiersXML;
+import modele.Plan;
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		
+		Controleur c = new Controleur();
+		
+	}
+
+
+}
