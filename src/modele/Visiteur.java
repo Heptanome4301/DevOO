d9aa4af9e5fa3e1477adab7ee2def6b3b144aad8
AyoubCh <aychioukh@gmail.com>
2015-11-11 01:32:35 +0100

    RenameVisiteur

--- src/modele/Visiteur.java
+++
@@ -1,8 +0,0 @@
-package modele;
-
-public interface Visiteur {
-	public void visite(Adresse a,boolean estEntrepot);
-	public void visite(Troncon c,boolean estDansTournee);
-	// public void visite(Tournee t);
-	
-}
