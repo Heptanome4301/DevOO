68b71275daa396588780366e27da411aa9b41fa0
maxime-christ <maxime.christ@insa-lyon.fr>
2015-11-12 18:26:12 +0100

    javadoc du controleur

--- src/controleur/ListeDeCmd.java
+++ src/controleur/ListeDeCmd.java
@@ -3,12 +3,12 @@ package controleur;
 import java.util.ArrayList;
 
 /**
- * Cette classe représente la pile d'actions de modification effectuées pour pouvoir implémenter un système
+ * Cette classe represente la pile d'actions de modification effectuees pour pouvoir implementer un systeme
  * de undo/redo.
  */
 public class ListeDeCmd {
         /**
-         * La liste des commandes effectuées.
+         * La liste des commandes effectuees.
          */
 	private ArrayList<Commande> listeCmd;
         /**
@@ -17,13 +17,17 @@ public class ListeDeCmd {
 	private int position;
 	
         /**
-         * 
+         * Le constructeur de la classe ne prend aucun parametre, il se contente d'initialiser les attributs.
          */
 	public ListeDeCmd() {
 		listeCmd = new ArrayList<>();
 		position = listeCmd.size();
 	}
 	
+        /**
+         * Ajoute une comande a la liste de commande.
+         * @param cmd la commande a ajouter.
+         */
 	public void ajoute(Commande cmd) {
                 for(int i = position; i<listeCmd.size()-1 ;i++)
                     listeCmd.remove(i);
@@ -31,20 +35,29 @@ public class ListeDeCmd {
 		position = listeCmd.size();
 	}
 	
-	public void undo() throws Exception {
+        /**
+         * Annule la derniere commande effectuee et decremente la position.
+         */
+	public void undo() {
 		if(position>0)	{
 			listeCmd.get(position-1).undoCmd();
 			position--;
 		}
 	}
 	
-	public void redo() throws Exception {
+        /**
+         * Reexecute la derniere action annulee et incrmente la position.
+         */
+	public void redo() {
 		if(position<listeCmd.size()) {
 			listeCmd.get(position).doCmd();
 			position++;
 		}
 	}
         
+        /**
+         * Vide la liste des commandes.
+         */
         public void clear() {
             listeCmd.clear();
             position = 0;
